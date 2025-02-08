ca1c6f56b9c794dcf699dde37af1091c994aa9d1
ashis <ashispanta100@gmail.com>
2025-02-08 21:50:06 +0900

    update environment variables for database connection and secret keys

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
-import os 
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:Ashis123@localhost:5432/productivity_app')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://productivity_app_e4h6_user:2j2EjIP64t5npmKVU2WoVs8V01CxXybA@dpg-cujkvlogph6c73bhsamg-a.singapore-postgres.render.com/productivity_app_e4h6')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecret')
